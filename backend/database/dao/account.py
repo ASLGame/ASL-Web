6c07f5d4a4b6a4e4ca3e8a673ae96f89acb06b32
Yamil <yamil.irizarry@upr.edu>
2022-02-27 18:10:36 -0400

    Created AccountDAO

--- backend/database/dao/account.py
+++ backend/database/dao/account.py
@@ -1,7 +1,15 @@
 from ..entity import account
+from api import db
 
 class AccountDao: 
     
     @staticmethod
     def getAllAccounts():
         return account.query.all()
+
+    @staticmethod
+    def create_account(json):
+        new_account = account(username=json['username'], password=json['password'], DOB=json['DOB'], first_name=json['first_name'], last_name=json['last_name'], 
+                                date_created=json['date_created'], date_updated=json['date_created'], role=json['role'])
+        db.session.add(new_account)
+        db.session.commit()
