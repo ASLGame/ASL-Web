72efafa0766095417142c3716c50cd7b0d9019de
Yamil <yamil.irizarry@upr.edu>
2022-02-27 16:07:59 -0400

    Created Score Handler

--- backend/api/handler/score_handler.py
+++ backend/api/handler/score_handler.py
@@ -1,3 +1,5 @@
+from curses.ascii import HT
+import http
 from database.dao import scoreDAO
 from flask import jsonify
 from api import HttpStatus
@@ -19,5 +21,55 @@ class ScoreHandler:
             score_dao = scoreDAO.create_score(json)
             if score_dao:
                 return jsonify(score_dao), HttpStatus.CREATED.value
+        except Exception as e:
+            return jsonify(reason="Server error", error=e.__str__()), HttpStatus.INTERNAL_SERVER_ERROR.value
+
+    def get_user_scores(account):
+        try:
+            score_dao = scoreDAO.get_user_scores(account)
+            result = []
+            for s in score_dao:
+                result.append(s)
+            return jsonify(result), HttpStatus.OK.value
+        except Exception as e:
+            return jsonify(reason="Server error", error=e.__str__()), HttpStatus.INTERNAL_SERVER_ERROR.value
+
+    def get_game_scores(game):
+        try:
+            score_dao = scoreDAO.get_game_scores(game)
+            result = []
+            for s in score_dao:
+                result.append(s)
+            return jsonify(result), HttpStatus.OK.value
+        except Exception as e:
+            return jsonify(reason="Server error", error=e.__str__()), HttpStatus.INTERNAL_SERVER_ERROR.value
+
+    def get_user_game_scores(json):
+        try:
+            score_dao = scoreDAO.get_user_game_scores(json)
+            result = []
+            for s in score_dao:
+                result.append(s)
+            return jsonify(result), HttpStatus.OK.value
+        except Exception as e:
+            return jsonify(reason="Server error", error=e.__str__()), HttpStatus.INTERNAL_SERVER_ERROR.value
+
+    def get_score_id(sid):
+        try:
+            score_dao = scoreDAO.get_score_id(sid)
+            if score_dao:
+                return jsonify(score_dao.as_dict()), HttpStatus.OK.value
+            else:
+                return jsonify("Score with ID: {} not found".format(sid)), HttpStatus.NOT_FOUND.value
+        except Exception as e:
+            return jsonify(reason="Server error", error=e.__str__()), HttpStatus.INTERNAL_SERVER_ERROR.value
+
+    def delete_score(sid):
+        try:
+            score_dao = scoreDAO.delete_score(sid)
+            if score_dao:
+                return jsonify("Score deleted successfully"), HttpStatus.OK.value
+            else:
+                return jsonify("Score not found"), HttpStatus.NOT_FOUND.value
         except Exception as e:
             return jsonify(reason="Server error", error=e.__str__()), HttpStatus.INTERNAL_SERVER_ERROR.value
