a95a6336810309a46d241620baf5c34a069d599d
christian alapont <christian.alapont@upr.edu>
2022-02-28 13:37:30 -0400

    Achievement type DAO, handler and routes.

--- backend/database/dao/__init__.py
+++ backend/database/dao/__init__.py
@@ -1,5 +1,7 @@
 from .account import AccountDao
 from .game import GameDao
+from .achievement_type import AchievementTypeDao
 
 accountDAO = AccountDao
 gameDao = GameDao
+achievementTypeDao = AchievementTypeDao
