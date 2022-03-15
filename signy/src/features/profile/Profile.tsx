b84aeeec34ff83340ed58f25efebecf8a60608ad
jose.biescas <jose.biescas@upr.edu>
2022-03-15 13:38:18 -0400

    Added temp profile picture editing, will need DB update probably

--- signy/src/features/profile/Profile.tsx
+++ signy/src/features/profile/Profile.tsx
@@ -1,17 +1,24 @@
 import { useAppDispatch } from "../../app/hooks";
 import styles from "./profile.module.css";
 import { ProfilePicture } from "./components/profilePicture/ProfilePicture";
+import { ChangeEvent, useState } from "react";
 
 export function Profile() {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const [profileImage, setProfileImage] = useState<string>("");
+  const [imageActive, setImageActive] = useState<boolean>(false);
 
-    return (
-        <section className={styles.container}>
-            <div className={styles.left}>
-                <ProfilePicture />
-            </div>
-            <div className={styles.right}>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.container}>
+      <div className={styles.left}>
+        <ProfilePicture
+          profileImage={profileImage}
+          setProfileImage={setProfileImage}
+          imageActive={imageActive}
+          setImageActive={setImageActive}
+        />
+      </div>
+      <div className={styles.right}></div>
+    </section>
+  );
 }
