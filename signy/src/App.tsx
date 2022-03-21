669a5036d423f3519d0359b2084ac095111b265b
Yamil <yamil.irizarry@upr.edu>
2022-03-21 16:32:16 -0400

    Created NavBar

--- signy/src/App.tsx
+++ signy/src/App.tsx
@@ -5,11 +5,24 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SignUp } from "./features/signup/SignUp";
 import Home from "./features/home/Home";
+import NavBar from "./components/NavBar";
+
+const navigation = {
+  brand: { name: "Signy", to: "/" },
+  links: [
+    { name: "Home", to: "/home" },
+    { name: "Sign-In", to: "/signin" },
+    { name: "Sign-Up", to: "/signup" },
+  ]
+}
+
+const { brand, links } = navigation;
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar brand={brand} links={links}/>
         <header className="App-header">
           <Routes>
             {/* 
