770cdf5306d177aaf60da7c0a3355c3ef1b4a4fc
jose.biescas <jose.biescas@upr.edu>
2022-03-29 11:51:04 -0400

    Added account_achievements, achievement models, and modified Account_Stats

--- backend/database/entity/account_stat.py
+++ backend/database/entity/account_stat.py
@@ -7,12 +7,13 @@ class AccountStat(db.Model):
 
     id = Column(Integer, primary_key=True)
     account_id = Column(ForeignKey('Account.id', ondelete='CASCADE'), nullable=False)
+    stats_id = Column(ForeignKey('Stats.id', ondelete='CASCADE'), nullable=False)
+    achievement_id = Column(ForeignKey('Achievement.id', ondelete='CASCADE'), nullable=False)
     value = Column(Integer, nullable=False)
-    has_achieved = Column(Boolean, nullable=False, server_default=text("false"))
-    date_achieved = Column(DateTime(True))
     date_created = Column(DateTime(True), nullable=False, server_default=text("CURRENT_TIMESTAMP"))
     date_updated = Column(DateTime(True), nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    stats_id = Column(ForeignKey('Stats.id', ondelete='CASCADE'))
+    
 
     account = relationship('Account')
     stats = relationship('Stat')
+    achievement = relationship('Achievement')
